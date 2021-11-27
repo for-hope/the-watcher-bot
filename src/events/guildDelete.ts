9c76ad810b7a3435221cb70eee840c46eedc3b11
Lamine <mlaminefetni@gmail.com>
2021-11-27 13:53:44 +0100

    guild delete fix

--- src/events/guildDelete.ts
+++ src/events/guildDelete.ts
@@ -1,9 +1,6 @@
 import { Guild } from "discord.js";
 
-import {
-  PortalRequest,
-  portalsByServerId,
-} from "../db/portalClient";
+import { PortalRequest, portalsByServerId } from "../db/portalClient";
 import { getTextChannel } from "../utils/bot_utils";
 
 module.exports = {
@@ -13,16 +10,21 @@ module.exports = {
     if (!portals) return;
 
     portals.forEach(async (portal) => {
-      const channelIdsOnPortal = portal.servers.map(
-        (server) => server.channel_id
-      );
+      const channelIdsOnPortal = portal.servers.map((server) => {
+        if (server.server_status === PortalRequest.approved) {
+          return server.channel_id;
+        }
+      });
 
+      if (!channelIdsOnPortal) return;
       channelIdsOnPortal.forEach(async (channelId) => {
-        const portalChannel = getTextChannel(guild.client, channelId);
-        if (portalChannel) {
-          portalChannel.send(
-            `**${guild.name}** \`${guild.id}\` has left the portal.`
-          );
+        if (channelId) {
+          const portalChannel = getTextChannel(guild.client, channelId);
+          if (portalChannel) {
+            portalChannel.send(
+              `**${guild.name}** \`${guild.id}\` has left the portal.`
+            );
+          }
         }
       });
 
