e4925e40e31f004695f3c7a95f9605b97d83de1e
Lamine <mlaminefetni@gmail.com>
2021-11-28 20:13:55 +0100

    made collectors work on production mode

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,12 +1,15 @@
 import { Client } from "discord.js";
 import { startCollectors } from "../services/start_collectors";
+import config from "../config.json";
 
 module.exports = {
   name: "ready",
   once: true,
   execute(client: Client) {
-    console.log(`Ready! Logged in as ${client.user?.tag}`);
+    console.log(`Ready! Logged in as ${client.user?.tag} in ${process.env.NODE_ENV} mode`);
     //start request collectors
-    startCollectors(client);
+    if (process.env.NODE_ENV === "production") {
+      startCollectors(client);
+    }
   },
 };
