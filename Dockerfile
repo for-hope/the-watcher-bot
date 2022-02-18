fffaa1ac9c10573b6f597563ac1c985935e066dd
Lamine <mlaminefetni@gmail.com>
2022-02-18 18:26:12 +0100

    added docker compose

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+# stage 1 build the bot
+FROM node:16.13-alpine
+WORKDIR /app
+COPY package.json .
+COPY package-lock.json .
+RUN npm install
+COPY . .
+RUN npm run build
+EXPOSE 5000
+CMD ["npm", "start"]
+
--- src/db/dbClient.ts
+++ src/db/dbClient.ts
@@ -2,6 +2,7 @@
 import mongoose from "mongoose";
 
 export const connectDb = async () => {
-  const m = await mongoose.connect("mongodb://localhost:27017/test");
-  console.log("Connected to MongoDB");
+  const connURL = process.env.MONGO_URL || "mongodb://localhost:27017/test";
+  const m = await mongoose.connect(connURL);
+  console.log(`Connected to MongoDB version ${m.version} at ${connURL}`);
 };
